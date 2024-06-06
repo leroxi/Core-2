1256fb694f46f94e65aa586016ba90bc35dd1856
Ленар <160050788+leroxi@users.noreply.github.com>
2024-06-06 13:25:10 +0300

    Update Generics.java

--- src/task/generics/Generics.java
+++ src/task/generics/Generics.java
@@ -34,20 +34,20 @@ public class Generics {
 
     //TODO: Сделайте рефакторинг метода так , что бы это метод мог работать не только с BigBox , а еще и с любыми его наследниками
     //  используйте для этого дженерики
-    public static <E extends BigBox> boolean checkBoxInList(List<E> list, E item) {
-        return list.contains(item);
+    public static <E extends BigBox> boolean checkBoxInList(List<E> boxList, E box) {
+        return list.contains(box);
     }
 
     //TODO: Таким же образом реализуйте метод добавления коробок в список при помощи дженериков . Тоесть , этот метод будет принимать
     // BigBox и его наследников и добавлять в список BigBox и его наследников
-    public static <Y extends BigBox> void addBoxToList(List<Y> list1, Y box) {
+    public static <Y extends BigBox> void addBoxToList(List<Y> boxList, Y box) {
         list1.add(box);
-        System.out.println(list1);
+        System.out.println(boxList);
     }
 
     //TODO: Так же реализуйте метод который будет проходится по списку с MediumBox и его наследников и выводить на экран содежимое каждого элемента
-    public static <F extends MediumBox> void printBoxList(List<F> list2) {
-        for (F i : list2) {
+    public static <F extends MediumBox> void printBoxList(List<F> boxList) {
+        for (F i : boxList) {
             System.out.println(i);
         }
     }
